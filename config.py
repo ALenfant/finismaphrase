82f2bd91de3ee99695bc2a82aa9ccb292bd11e39
Antonin Lenfant <antonin.lenfant@datadoghq.com>
2021-04-12 18:34:18 +0200

    Set worker processes count on Dokku, add tweepy

--- config.py
+++ config.py
@@ -5,7 +5,13 @@ import os
 
 logger = logging.getLogger()
 
+
 def create_api():
+    # Ensure we have all the necessary info
+    for env_key in ("TWITTER_API_KEY", "TWITTER_SECRET_KEY", "TWITTER_BOT_ACCESS_KEY", "TWITTER_BOT_ACCESS_SECRET"):
+        if env_key not in os.environ:
+            raise Exception("Missing environment key %s" % env_key)
+
     consumer_key = os.getenv("TWITTER_API_KEY")
     consumer_secret = os.getenv("TWITTER_SECRET_KEY")
     access_token = os.getenv("TWITTER_BOT_ACCESS_KEY")
@@ -13,8 +19,7 @@ def create_api():
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth, wait_on_rate_limit=True,
-        wait_on_rate_limit_notify=True)
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
     try:
         api.verify_credentials()
     except Exception as e:
